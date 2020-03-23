f22b938c272ce1722235922c1deb8aca5fcb6f33
HDhian <56302681+HDhian@users.noreply.github.com>
2020-03-23 17:03:48 +0100

    naam op c#

--- ProjectB/ProjectB/Program.cs
+++ ProjectB/ProjectB/Program.cs
@@ -7,7 +7,7 @@ namespace ProjectB
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
+            string name = "Hendrik"
         }
     }
 }
