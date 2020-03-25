575bd0f061d6b96023edc90a89d4d82af854757f
HDhian <56302681+HDhian@users.noreply.github.com>
2020-03-25 12:01:19 +0100

    Klant Info

---
+++ Klanten_gegevens/Klanten_gegevens/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Klanten_gegevens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Tafel1 = Tuple.Create("", 0, "", 1 - 1 - 2020, 1 - 00, "");
+
+            var Tafel2 = Tuple.Create("", 0, "", 1 - 1 - 2020, 1 - 00, "");
+
+            var Tafel3 = Tuple.Create("", 0, "", 1 - 1 - 2020, 1 - 00, "");
+
+            var Tafel4 = Tuple.Create("", 0, "", 1 - 1 - 2020, 1 - 00, "");
+
+            var Tafel5 = Tuple.Create("", 0, "", 1 - 1 - 2020, 1 - 00, "");
+
+            var Tafel6 = Tuple.Create("", 0, "", 1 - 1 - 2020, 1 - 00, "");
+
+            var Tafel7 = Tuple.Create("", 0, "", 1 - 1 - 2020, 1 - 00, "");
+
+            var Tafel8 = Tuple.Create("", 0, "", 1 - 1 - 2020, 1 - 00, "");
+
+            var Tafel9 = Tuple.Create("", 0, "", 1 - 1 - 2020, 1 - 00, "");
+
+            var Tafel10 = Tuple.Create("", 0, "", 1 - 1 - 2020, 1 - 00, "");
+
+
+
+
+            
+        }
+    }
+}
