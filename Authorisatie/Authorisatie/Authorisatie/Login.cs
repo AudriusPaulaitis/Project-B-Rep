ae67f2100d569b3398db9cd18d236ff41e045828
Audrynass <55562216+AudriusPaulaitis@users.noreply.github.com>
2020-03-26 17:37:48 +0100

    Authorisatie
    Authorisatie bestaat uit een Main functie + Login Functie

---
+++ Authorisatie/Authorisatie/Authorisatie/Login.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Authorisatie
+{ 
+
+
+
+
+    class Login
+    {
+    //Mini Mini database
+        string[] werknemers = new string[] { "#3342", "#4423", "#4552" };
+        string[] wachtwoorden = new string[] { "vasd3", "elle$2", "bolpw4$" };
+    //Method voor inlogverificatie
+        public bool loginScreen()
+        {
+            Console.WriteLine("Voer uw werknemers ID en wachtwoord in");
+            Console.WriteLine("--------------------------------------");
+            Console.Write("Werknemers ID: ");
+            string werknemersId = Console.ReadLine();
+            Console.Write("Wachtwoord: ");
+            string wachtwoord = Console.ReadLine();
+
+            if (werknemers.Contains(werknemersId) && wachtwoorden.Contains(wachtwoord))
+            {
+                int we = Array.IndexOf(werknemers, werknemersId);
+                int wa = Array.IndexOf(wachtwoorden, wachtwoord);
+                if (we == wa)
+                {
+                    return true;
+                }
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+    }
+}
