d7c5481d938b08966ba702894ea5cf04be3b1ed1
Audrynass <55562216+AudriusPaulaitis@users.noreply.github.com>
2020-03-26 17:42:08 +0100

    main functie
    Hier kunnen jullie functies/methoden in.

---
+++ Authorisatie/Authorisatie/Authorisatie/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authorisatie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            Login login = new Login();
+            bool what = login.loginScreen();
+            if (what == false)
+            {
+                Console.WriteLine("Verkeerde inloggegevens");
+                login.loginScreen();
+            }
+        }
+    }
+}
