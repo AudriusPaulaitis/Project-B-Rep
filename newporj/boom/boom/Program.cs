7952b9089974edde39f2788989fb1bf72de5dd59
waakwaan <56305864+waakwaan@users.noreply.github.com>
2020-03-31 20:42:45 +0200

    main pagina
    !

---
+++ newporj/boom/boom/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+ 
+namespace boom
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                bool Menu = true;
+                while (Menu)
+                {
+                    Menu = MainMenu();
+                }
+            }
+            private static bool MainMenu()
+            {
+
+                Console.Write("\r\nKies een optie en druk enter: \n");
+                Console.WriteLine("1) Reserveringscherm");
+                Console.WriteLine("2) Menu");
+                Console.WriteLine("3) Autorisatiescherm");
+                Console.WriteLine("4) Afsluiten");
+                Console.Write("\r\nKies een optie: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Reserveringscherm();
+                        return true;
+                    case "2":
+                        Menus();
+                        return true;
+                    case "3":
+                        Author();
+                        return true;
+                    case "4":
+                        return false;
+                    default:
+                        return true;
+                    
+                }
+            }
+            private static void Reserveringscherm()
+            {
+                Console.Clear();
+                Console.WriteLine("Binnenkort: reserveringscherm");
+
+            }
+        private static void Menus()
+        {
+            Console.Clear();
+            Console.WriteLine("binnenkort: Menukaart");
+
+        }
+        private static void Author()
+        {
+            Console.Clear();
+            Console.WriteLine("binnenkort: inlogpagina");
+
+        }
+        }
+    }
