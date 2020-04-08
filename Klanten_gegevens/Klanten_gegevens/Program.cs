d3394e5370b624e9e79b5a99e47514f827d5e4a0
HDhian <56302681+HDhian@users.noreply.github.com>
2020-04-08 10:53:23 +0200

    Program comments

--- Klanten_gegevens/Klanten_gegevens/Program.cs
+++ Klanten_gegevens/Klanten_gegevens/Program.cs
@@ -6,14 +6,15 @@ namespace Klanten_gegevens
     {
         static void Main(string[] args)
         {
+            //call todays reservation screen 
             ReserData gegevens = new ReserData();
             bool vandaag = gegevens.ReservationScreen();
 
-
+            //call reservation screen on chosen date
             DateReserData datedata = new DateReserData();
             string check = datedata.ReservationScreenDate();
 
-
+            //return to todays reservation
             if (check == "0")
             {
                 gegevens.ReservationScreen();
