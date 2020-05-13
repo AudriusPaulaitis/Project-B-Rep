e2acebfb1a05d4f34a3f1395411aa4451d8b4c70
Audrynass <55562216+AudriusPaulaitis@users.noreply.github.com>
2020-05-13 14:26:02 +0200

    program.cs toegevoegd

---
+++ Restaurant Case Groep1/Restaurant Case Groep1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_Case_Groep1
+{
+    //class Program runt het hele programma
+    class Program
+    {
+        // In de main kan de user door verschillende keuzes navigeren
+        static void Main(string[] args)
+        {
+            Navigation nav = new Navigation();
+            nav.navigation();
+        }
+    }
+}
