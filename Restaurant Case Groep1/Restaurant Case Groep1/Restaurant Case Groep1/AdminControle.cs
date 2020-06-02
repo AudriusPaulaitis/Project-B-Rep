3d6553b5819260d4acb2457d1a8157cb52168813
waakwaan <56305864+waakwaan@users.noreply.github.com>
2020-06-02 19:09:15 +0200

    admin controle
    admin controle toegevoegd in de main (hopelijk werkt alles zonder probleem lol)

---
+++ Restaurant Case Groep1/Restaurant Case Groep1/Restaurant Case Groep1/AdminControle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant_Case_Groep1
+{
+    public class EmptyClass
+    {
+        public class AdminControle
+        {
+           
+            
+
+            public List<string> Table_1 = new List<string>();
+            public List<string> Table_2 = new List<string>();
+            public List<string> Table_3 = new List<string>();
+            public List<string> Table_4 = new List<string>();
+            public List<string> Table_5 = new List<string>();
+            public List<string> Table_6 = new List<string>();
+            public List<string> Table_7 = new List<string>();
+            public List<string> Table_8 = new List<string>();
+            public List<string> Table_9 = new List<string>();
+            public List<string> Table_10 = new List<string>();
+            public List<string> Table_11 = new List<string>();
+            public List<string> Table_12 = new List<string>();
+            public List<string> Table_13 = new List<string>();
+            public List<string> Table_14 = new List<string>();
+            public List<string> Table_15 = new List<string>();
+
+
+            public void tablePage()
+            //Hier is om keuzes te maken als medewerker een bestelling op te nemen.
+            {
+                Console.Clear();
+                Console.WriteLine("Admin pagina");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Selecteer een tafel (tafel 1- 15");
+                int TableChoise = Console.Read();
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Kies 1 om bestellingen op te nemen");
+                Console.WriteLine("Kies 2 om bestelling te verwijderen");
+                Console.WriteLine("Kies 3 om de tafel vrij te maken of kies 0 om terug te gaan.");
+                int ChoiseOrder = Console.Read();
+
+            }
+
+        }
+    }
+
+}
+ 
