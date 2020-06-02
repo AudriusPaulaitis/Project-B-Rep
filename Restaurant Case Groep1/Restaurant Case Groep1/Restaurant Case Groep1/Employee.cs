27aedde91b5b6ee193cb3993e863f6397c647d9a
Audrynass <55562216+AudriusPaulaitis@users.noreply.github.com>
2020-06-02 11:39:19 +0200

    Key navigatie
    Key navigatie toegepast

---
+++ Restaurant Case Groep1/Restaurant Case Groep1/Restaurant Case Groep1/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_Case_Groep1
+{
+    // Employee class maakt een employee object
+    public class Employee
+    {
+        public string WorkId { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ Restaurant Case Groep1/Restaurant Case Groep1/Restaurant Case Groep1/Navigation.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Restaurant_Case_Groep1
+{
+	public class Navigation
+	{
+        int index = 0;
+		public void navigation()
+		{
+            Console.Clear();
+            Console.WriteLine("Navigatiescherm");
+            Console.WriteLine("---------------");
+            Console.CursorVisible = false;
+            List<string> Options = new List<string>()
+            {
+                "Reserveringsscherm",
+                "Menuscherm",
+                "Loginscherm",
+                "Exit"
+            };
+
+            while (true)
+            {
+                string SelectedOption = Selector(Options);
+                ReservationManager reservationManager = new ReservationManager();
+                Menus menus = new Menus();
+                Login login = new Login();
+                AdminPage page = new AdminPage();
+
+                if (SelectedOption == "Reserveringsscherm")
+                {
+                    reservationManager.reservationMenu();
+                    Console.Read();
+                }
+                else if (SelectedOption == "Menuscherm")
+                {
+                    menus.menuStart();
+                    Console.Read();
+                }
+                else if (SelectedOption == "Loginscherm")
+                {
+                    if (login.loginScreen())
+                    {
+                        page.PrintToday();
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Verkeerde inloggevens");
+                    }
+
+                }
+                else if (SelectedOption == "Exit")
+                {
+                    Environment.Exit(0);
+                }
+
+            }   
+        }
+        public string Selector(List<string> Options)
+        {
+            Console.Clear();
+            Console.WriteLine("Navigatiescherm");
+            Console.WriteLine("---------------");
+            for (int i = 0; i < Options.Count; i++)
+            {
+                if (i == index)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(Options[i]);
+                }
+                else
+                {
+                    Console.WriteLine(Options[i]);
+                }
+                Console.ResetColor();
+            }
+            ConsoleKeyInfo PressedKey = Console.ReadKey();
+            Console.Clear();
+            if (PressedKey.Key == ConsoleKey.UpArrow && index >= 0)
+            {
+                index -= 1;
+            }
+            else if (PressedKey.Key == ConsoleKey.DownArrow && index < Options.Count)
+            {
+                index += 1;
+            }
+            else if (PressedKey.Key == ConsoleKey.Enter)
+            {
+                return Options[index];
+            }
+            else
+            {
+                return "";
+            }
+            return "";
+        }
+
+    }
+}
+
+
