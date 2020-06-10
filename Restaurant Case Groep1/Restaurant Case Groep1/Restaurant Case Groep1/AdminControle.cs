021b432b46041cd5ab1f896bfb6d0bd51df06ccb
waakwaan <56305864+waakwaan@users.noreply.github.com>
2020-06-10 12:06:36 +0200

    Update AdminControle.cs

--- Restaurant Case Groep1/Restaurant Case Groep1/Restaurant Case Groep1/AdminControle.cs
+++ Restaurant Case Groep1/Restaurant Case Groep1/Restaurant Case Groep1/AdminControle.cs
@@ -44,9 +44,9 @@ namespace Restaurant_Case_Groep1
                    Console.WriteLine("Admin pagina");
                    Console.WriteLine("--------------------------------------");
                    Console.WriteLine("Selecteer een tafel (tafel 1- 15");
-                   Console.WriteLine("Kies "0" om terug te gaan");
-                   string TableChoise = Console.Read();
-                   if (TableChiose == "0")
+                   Console.WriteLine("Kies 0 om terug te gaan");
+                   int TableChoise = Console.Read();
+                   if (TableChoise == 0)
                    {
                       break;
                    }
@@ -59,20 +59,20 @@ namespace Restaurant_Case_Groep1
                          Console.WriteLine("Kies 1 om bestellingen op te nemen");
                          Console.WriteLine("Kies 2 om bestelling te verwijderen");
                          Console.WriteLine("Kies 3 om de tafel vrij te maken of kies 0 om terug te gaan.");
-                         string ChoiseOrder = Console.Read();
-                         if (ChoiseOrder == "0")
+                         int ChoiseOrder = Console.Read();
+                         if (ChoiseOrder == 0)
                          {
                             break;
                          }
-                         if (ChoiseOrder == "1")
+                         if (ChoiseOrder == 1)
                          {
                             Console.Clear();
-                            Console.Writeline("Selecteer een nummer om een bestelling te nemen");
-                            Console.Writeline("Kies "0" om terug te gaan");
-                            string OrderMenu = Console.ReadLine();
-                            if (OrderMenu == "0")
+                            Console.WriteLine("Selecteer een nummer om een bestelling te nemen");
+                            Console.WriteLine("Kies 0 om terug te gaan");
+                            int OrderMenu = Console.Read();
+                            if (OrderMenu == 0 )
                             {
-                               Console.Clear();
+                                Console.Clear();
                             }
                          }
                       }
