080a9c4ff53484d98183ad0346fad23d0995a533
Beau <0989647@hr.nl>
2020-06-10 12:23:53 +0200

    Ewa ja

--- Restaurant Case Groep1/Restaurant Case Groep1/Restaurant Case Groep1/Dish.cs
+++ Restaurant Case Groep1/Restaurant Case Groep1/Restaurant Case Groep1/Dish.cs
@@ -10,12 +10,13 @@ namespace Restaurant_Case_Groep1
         public int Prijs { get; set; }
         public string Beschrijving { get; set; }
         public string Allergenen { get; set; }
+        public int GerechtID { get; set; }
 
        // samen met de peercoach ben ik op dit gekomen. Als ik de json printen kwam er alleen een locatie uit en niet wat er in de json stond.
        // Door de functies binnen de string te returnen kan ik het euro teken voor de int prijs te zetten.
         public override string ToString()
         {
-            return $"{this.Naam}: €{this.Prijs} \n {this. Beschrijving} \n {this.Allergenen} ";
+            return $" Id: {this.GerechtID} \n {this.Naam}: €{this.Prijs} \n {this. Beschrijving} \n {this.Allergenen} ";
         }
         
 
